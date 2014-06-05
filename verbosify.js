d3aaf500ebcc267786e6eec85b1a6acceda9cd66
Spencer Allen <spencer@apexhire.com>
2014-06-05 13:57:16 -0700

    Updated Verbosify.js
    Added a json entry to replace the switch. 
    contains the option to return random synonyms or the longest possible synonyms.

--- verbosify.js
+++ verbosify.js
@@ -2,63 +2,66 @@ function textProcess(form) {
         var apiCalls = [];
         var textInput = form.inputbox.value;
         var inputArray = textInput.split(" ");
+
         for (i = 0; i < inputArray.length; i++) {
             var t_url = "http://api.wordnik.com/v4/word.json/" + inputArray[i] + "/relatedWords?useCanonical=true&relationshipTypes=synonym&limitPerRelationshipType=20&api_key=13ee2e8b568303c9950050dad6205899a9cf91816cad42252";
             apiCalls.push(t_url);
 
         }
+
         console.log("apiCalls", apiCalls);
 
+        var words = {
+            'you': 'thou',
+            'my':'mine',
+            'pants': 'panteloons',   
+            'man':'gentlefellow',   
+            'you':'you, yourself',   
+            'have':'possess',   
+            'I':'mine own person',
+            'be':'exist as',
+            'and':'in addition to',
+            'of':'concerning',
+            'an instance of':'a',
+            'it':'the entity which I shall refer to as it',
+            'he':'the particular male in question',
+            'do':'accomplish',
+            'on':'upon',
+            'say':'verbalize ones own mouth',
+            'this':'the particular object in question',
+            'they':'all of those',
+            'at':'in the vicinity of',
+             
+        };
+
         for (j = 0; j < apiCalls.length; j++) {
-            switch (inputArray[j]){
-            case "you":
-                document.write("thou", " ");
-                j++;
-                break;
-            case "my":
-                document.write("mine", " ");
-                j++;
-                break;
-            case "pants":
-                document.write("panteloons", " ");
-                j++;
-                break;
-            case "man":
-                document.write("gentlefellow", " ");
-                j++;
-                break;
-            case "in":
-                document.write("therewithin", " ");
-                j++;
-                break;
-            case "have":
-                document.write("possess", " ");
-                j++;
-                break;
-            case "I":
-                document.write("mine own person", " ");
-                j++;
-                break;
+            var replacement = words[inputArray[j]];
+            
+            if (typeof replacement !== 'undefined') {
+            	document.write(replacement, ' ');
+            	continue;
             }
+
             var xmlHttp = null;
             xmlHttp = new XMLHttpRequest();
             xmlHttp.open("GET", apiCalls[j], false);
             xmlHttp.send(null);
 
             var wordnikResponse = xmlHttp.responseText;
-                console.log(wordnikResponse);
-                
+            console.log(wordnikResponse);
+
             if (wordnikResponse == "[]") {
                 document.write(inputArray[j], " ");
             } else {
-                         
-            var parsedResponse = JSON.parse(wordnikResponse);
-            var responseArray = parsedResponse[0].words;
+
+                var parsedResponse = JSON.parse(wordnikResponse);
+                var responseArray = parsedResponse[0].words;
                 console.log(responseArray);
-            var longest = responseArray.sort(function(a, b) {return b.length - a.length;})[0];
-            var rand = responseArray[Math.floor(Math.random() * responseArray.length)];
-               document.write(rand, " ");
-            }   
-            
+                var longest = responseArray.sort(function(a, b) {
+                    return b.length - a.length;
+                })[0];
+                var rand = responseArray[Math.floor(Math.random() * responseArray.length)];
+            document.write(longest, " ");
         }
-    }
+
+}
